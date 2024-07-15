6f13acba09354ccd6fa7b2a346b96d1ef17976fb
Filipe <sogarnsystems@gmail.com>
2024-07-15 10:24:20 -0400

    Set up HUD and started mob groups

--- Main.cs
+++ Main.cs
@@ -23,6 +23,7 @@ public partial class Main : Node
 	{
 		GetNode<Timer>("MobTimer").Stop();
 		GetNode<Timer>("ScoreTimer").Stop();
+		GetNode<HUD>("HUD").ShowGameOver();
 	}
 
 	// New game settings
@@ -35,12 +36,17 @@ public partial class Main : Node
 		player.Start(startPosition.Position);
 
 		GetNode<Timer>("StartTimer").Start();
+
+		var hud = GetNode<HUD>("HUD");
+		hud.UpdateScore(_score);
+		hud.ShowMessage("Get Ready!");
 	}
 
 	// Timer settings
 	private void OnScoreTimerTimeout()
 	{
 		_score++;
+		GetNode<HUD>("HUD").UpdateScore(_score);
 	}
 
 	private void OnStartTimerTimeout()
